d200410fc11aba97238d08e1b780f5989580cbbf
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-09-16 23:35:54 -0300

    tasks included

---
+++ Calculo_lados.py
@@ -0,0 +1,11 @@
+""" Escreva um programa para ler o número de lados (NumLados) de um polígono regular e a medida 
+do lado (MedLado)
+− Se o número de lados for igual a 3 imprima “TRIÂNGULO”, calcule e mostre a área do triângulo (Use 
+o Teorema de HERON para calcular a área do triangulo somente com lados, pesquise no google)
+− Se o número de lados for igual a 4 imprima “QUADRADO”, calcule e mostre a área do quadrado. 
+− Se o número de lados for igual a 5 imprime “PENTÁGONO”, calcule e mostre a área do pentágono. 
+(Pesquise no google com se calcula a area de um PENTAGONO)
+Acrescente as seguintes mensagens ao exercício 1 conforme o caso.
+− Caso o número de lados seja inferior a 3 escrever NÃO É UM POLÍGONO.
+− Caso o número de lados seja superior a 5 escrever POLÍGONO NÃO IDENTIFICADO. 
+"""
