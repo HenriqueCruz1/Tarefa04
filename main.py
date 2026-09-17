d09837692eb722b2fea5356de29adb6f0d0bc2f7
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-09-16 22:58:10 -0300

    POO

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello World")
-print("I just to try remenber..")
+class Tentativa:
+    def __init__(self):
+        self.pessoa = ""
+        self.tentativa = 0
+
+    def tentar(self):
+        self.tentativa = self.tentativa +1
+
+
+
