308caf529a6e7bd6582ae1c94ef5e1bee6fde3f7
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-09-17 22:31:16 -0300

    exercicio calculo de lados com Heron finalizado

--- Calculo_lados.py
+++ Calculo_lados.py
@@ -9,16 +9,35 @@ Acrescente as seguintes mensagens ao exercício 1 conforme o caso.
 Caso o número de lados seja inferior a 3 escrever NÃO É UM POLÍGONO.
 Caso o número de lados seja superior a 5 escrever POLÍGONO NÃO IDENTIFICADO. 
 """
+import math
 
 NumLados = int(input("Digite o número de lados da figura a ser analisada: "))
+MedLado = float(input("Digite a medida do lado: "))
 
 if NumLados == 3:
+
     print("TRIÂNGULO")
+
+    s = (MedLado * 3) / 2
+
+    Area = math.sqrt(
+        s * (s - MedLado) * (s - MedLado) * (s - MedLado)
+    )
+
+    print(f"Área: {Area:.2f}")
+
 elif NumLados == 4:
+
     print("QUADRADO")
+
 elif NumLados == 5:
-    print("PENTÂGONO")
+
+    print("PENTÁGONO")
+
 elif NumLados < 3:
+
     print("NÃO É UM POLÍGONO")
+
 elif NumLados > 5:
+
     print("POLÍGONO NÃO IDENTIFICADO")
