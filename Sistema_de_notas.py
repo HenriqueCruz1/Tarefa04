96a24a7d8972f9feb1baf1a4bf53040d85059ffc
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-09-17 22:27:03 -0300

    Exercicio sistema de notas finalizado

--- Sistema_de_notas.py
+++ Sistema_de_notas.py
@@ -8,3 +8,27 @@ Cuide para que as notas de entrada assim como as faltas não sejam
 valores negativos. 
 '''
 
+while True:
+    nota_A = float(input("Digite a primeira nota: "))
+    nota_B = float(input("Digite a segunda nota: "))
+    quantidade_faltas = int(input("Digite a quantidade de faltas: "))
+    media = (nota_A + nota_B) / 2
+
+    if quantidade_faltas > 0 and media > 0:
+        break
+    else:
+        print("Entrada invalida, há valores negativos")
+
+if quantidade_faltas > 20:
+    print(f"quantidade de faltas: {quantidade_faltas}, reprovado por faltas")
+        
+if media <= 3:
+    print(f"media: {media}, reprovado por notas")
+elif media > 3 and media <= 5:
+    print(f"media: {media}, Aluno de Recuperação")
+elif media > 5 and media <= 6:
+    print(f"media: {media}, Aluno de Exame")
+else:
+    print(f"media: {media}, Aluno aprovado")
+
+
